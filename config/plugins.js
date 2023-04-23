482a03cc6aa1ac3c6c09bbc3a068a3891bef672a
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-04-22 23:25:57 -0500

    ajustes

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        "https://api.vercel.com/v1/integrations/deploy/prj_2o66oiAcdTKwyHqWPBCVoUQHMDXe/INK8RN4fmA",
       apiToken: "<vercel-api-token>",
       appFilter: "your-app-name-on-vercel",
       teamFilter: "your-team-id-on-vercel",
