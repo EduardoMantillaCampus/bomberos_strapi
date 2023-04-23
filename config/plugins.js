2045470448ebcc27f40d4f1228bb1c58c6a55018
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-04-23 00:05:34 -0500

    ajustes

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,7 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_2o66oiAcdTKwyHqWPBCVoUQHMDXe/INK8RN4fmA",
+      deployHook:"https://api.vercel.com/v1/integrations/deploy/prj_2o66oiAcdTKwyHqWPBCVoUQHMDXe/INK8RN4fmA",
       apiToken: "r4mJ2dGQY7WNl5AgqMyu1FzM",
       appFilter: "bomberos-strapi",
       teamFilter: "Ods77wdwdoqCWEZUZSjpZyOL",
